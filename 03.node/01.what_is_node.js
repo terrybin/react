e64f5543addfaeca16b15ff381c17ee54bb214fd
Terry <god941212@naver.com>
2024-04-10 21:00:11 +0900

    node start

---
+++ 03.node/01.what_is_node.js
@@ -0,0 +1,15 @@
+// node.js
+//  - 웹 브라우저가 아닌 환경에서도 자바스크립트를 구동할 수 있도록 도와주는
+//  - 자바스크립트 실행환경 (Javascript 런타임) 이다.
+
+// - ex) 게임기(node), 게임(javascript)
+
+// npm 
+//  - node package manager
+//  - package를 모아둘 폴더를 별도 생성한다(현재 project에서는 package_manager로 생성함)
+
+//  - 터미널에서 package_manager로 디렉터리를 변경한 후 npm init을 사용하여 npm을 초기화한다.
+//      => 정상적으로 완료되면 package_manager 폴더에 package.json 파일이 생성된다.
+
+//  - node로 js 파일을 실행하려면
+//      => 터미널에 node (파일명.js)를 작성하여 실행한다.
